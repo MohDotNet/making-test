5d31b8ec67fc3d38546eae51d25fe15c6d2979ae
ameer <ameer@SAYEDPC>
2024-07-21 12:16:20 +0200

    Add project files.

---
+++ making-test/MaskingService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Services;
+
+namespace making_test
+{
+    public static class MaskingService
+    {
+        public static string ToMasked(this string textToMask)
+        {
+            int visibleLength = 4;
+            string result = string.Empty;
+
+            if (string.IsNullOrEmpty(textToMask))
+            {
+                return result;
+            }
+
+            int length = textToMask.Length;
+            result = textToMask.Substring(0, visibleLength) + new string('*',length - visibleLength);
+            
+            return result;
+        }
+    }
+}
